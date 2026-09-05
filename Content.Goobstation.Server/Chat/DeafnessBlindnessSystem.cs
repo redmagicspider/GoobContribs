1284df4240a0025d3dcbda22788e6f066c14f33f
redmagicspider <mantiss482@gmail.com>
2026-09-04 23:29:06 -0300

    Removed comments

--- Content.Goobstation.Server/Chat/DeafnessBlindnessSystem.cs
+++ Content.Goobstation.Server/Chat/DeafnessBlindnessSystem.cs
@@ -52,11 +52,9 @@ public sealed class DeafnessBlindnessSystem : EntitySystem
     }
     private void OnBlindnessOverrideInRange(Entity<PermanentBlindnessComponent> ent, ref ChatMessageOverrideInRange args)
     {
-        // Goob edit start
         if (!args.RequiresSight)
             return;
         if (ent.Comp.Blindness <= 0)
             args.Cancel();
-        // Goob edit end
     }
 }
